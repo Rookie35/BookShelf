cc262ac321c4dcb13e01f0ee73fe8ebc76f590c1
754119201@qq.com <754119201@qq.com>
2019-05-07 13:04:24 +0800

    完成DrawerLayout框架搭建

---
+++ BookShelf/app/src/main/java/com/example/bookshelf/ContentFragment.java
@@ -0,0 +1,25 @@
+package com.example.bookshelf;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Created by ZKF on 2019/5/7.
+ */
+
+public class ContentFragment extends Fragment {
+    private TextView tv_content;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        View view=inflater.inflate(R.layout.fg_content,container,false);
+        tv_content=(TextView)view.findViewById(R.id.tv_content);
+        String text=getArguments().getString("text");
+        tv_content.setText(text);
+        return view;
+    }
+}
